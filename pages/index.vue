bbc342674d39854844cba12fe8e853836a5090cb
Vince489 <vincentcornelius489@gmail.com>
2023-07-17 18:36:25 -0400

    You did it!

--- pages/index.vue
+++ pages/index.vue
@@ -1,81 +1,23 @@
 <template>
 
 
-  <section class="flex flex-col justify-center">
+  <section >
 
     <div class="flex justify-center pb-10">
       <h1 class="text-3xl font-semibold">IN THE SPOTLIGHT</h1>
     </div>
 
-    <div class="flex justify-center">
-
-      <div class="flex flex-col-reverse max-w-6xl lg:flex-row p-4 pb-6 mx-auto">
-
-        <div class="one lg:w-1/2 bg-gray-200 rounded-b-lg ">
-
-          <div class="p-10">        
-            <h2 class="pb-5">Lamar Wilson</h2>
-            <h3 class="pb-3">XBOX - Metabox Promotions</h3>
-            <p class="pb-5">Middleweight prospect advances to (6-0-0) with his Unanimous Decision win over Gomez.</p>
-            <button class="px-4 py-2 rounded-full bg-blue-500 text-white">FULL FIGHT</button>
-          </div>
-
-        </div>
-        
-        <div class="two lg:w-1/2">
-          <div>
-            <img class="rounded-t-lg" src="https://virtronesports.com/img/lee-ko.png" alt="">
-          </div>
-        </div>
-      </div>
+    <div>
+      <Spotlight1 />
     </div>
 
-    <div class="flex justify-center">
-
-      <div class="flex flex-col max-w-6xl lg:flex-row p-4 pb-6 mx-auto">
-
-        <div class="two lg:w-1/2">
-          <div>
-            <img class="rounded-tl-lg" src="https://virtronesports.com/img/lee-ko.png" alt="">
-          </div>
-        </div>     
-
-        <div class="one lg:w-1/2 bg-gray-200 lg:rounded-bl-lg-none rounded-bl-lg rounded-br-lg">
-
-          <div class="p-10">        
-            <h2 class="pb-5">Lamar Wilson</h2>
-            <h3 class="pb-3">XBOX - Metabox Promotions</h3>
-            <p class="pb-5">Middleweight prospect advances to (6-0-0) with his Unanimous Decision win over Gomez.</p>
-            <button class="px-4 py-2 rounded-full bg-blue-500 text-white">FULL FIGHT</button>
-          </div>
-
-        </div>
-        
-      </div>
+    <div>
+      <Spotlight2 />
     </div>
 
-    <div class="flex justify-center">
-
-      <div class="flex flex-col-reverse max-w-6xl lg:flex-row p-4 pb-6 mx-auto">
-
-        <div class="one lg:w-1/2 bg-gray-200 lg:rounded-l-lg">
-
-          <div class="p-10">        
-            <h2 class="pb-5">Lamar Wilson</h2>
-            <h3 class="pb-3">XBOX - Metabox Promotions</h3>
-            <p class="pb-5">Middleweight prospect advances to (6-0-0) with his Unanimous Decision win over Gomez.</p>
-            <button class="px-4 py-2 rounded-full bg-blue-500 text-white">FULL FIGHT</button>
-          </div>
-
-        </div>
-        
-        <div class="two lg:w-1/2">
-          <div>
-            <img class="lg:rounded-r-lg" src="https://virtronesports.com/img/lee-ko.png" alt="">
-          </div>
-        </div>
-      </div>
-    </div> 
+    <div>
+      <Spotlight1 />
+    </div>
 
   </section>
 
