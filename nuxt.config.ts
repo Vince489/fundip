229a46b098f6a6ec5a51cdd47e67522d1cf7f64d
Vince489 <vincentcornelius489@gmail.com>
2023-08-28 18:11:12 -0400

    removed nitro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-  nitro: {
-    plugins: ['~/server/index.ts'],
-  },
-
-  runtimeConfig: {
-    // Will be available in both server and client
-    mongodbUri: process.env.MONGODB_URI
-  }
-
+  modules: ['@nuxtjs/tailwindcss']
 })
--- server/api/boxers/index.get.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler((event) => {
-  return {
-    boxers: 'boxers'
-  }
-})
-
-// set a key value pair in cookies
-
-
