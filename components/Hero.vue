7fd56972188ae65bbc26529a5a321bc0343ce768
Vince489 <vincentcornelius489@gmail.com>
2023-09-12 19:56:10 -0400

    Hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -6,5 +6,23 @@
 
     <div class="overlay absolute inset-0 bg-black opacity-40"></div>
 
+    <div class="container mx-auto pt-56">
+      <div class="flex justify-center">
+        <div class="lg:w-2/3 text-center relative">
+          <div class="space-y-6 mb-10">
+            <h2 class="text-white text-4xl md:text-5xl lg:text-6xl leading-tight capitalize font-bold">
+              SIM BOXING LEAGUE
+            </h2>
+            <p class="text-gray-300 text-base md:text-lg">
+              Don't leave it to the judges.
+            </p>
+            <button class="px-4 py-2 rounded-full bg-blue-500 text-white">
+              Join Now!
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </section>
 </template>
--- pages/login.vue
+++ pages/login.vue
@@ -9,17 +9,13 @@
           <label class="block text-gray-700 text-sm font-bold mb-2" for="gamerTag">
             Gamertag
           </label>
-          <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="gamerTag" type="text" placeholder="Gamertag" v-model="gamerTag">
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="gamerTag" type="text" placeholder="Gamertag" v-model="gamerTag">
         </div>
         <div class="mb-4">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
             Password
           </label>
-          <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password" type="password" placeholder="******************" v-model="password">
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" v-model="password">
           <p class="hidden text-red-500 text-xs italic">Please choose a password.</p>
         </div>
         <div class="flex justify-center">
