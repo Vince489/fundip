6bc56476e0659cbae02cc8b14bb0d9cf06752cb3
Vince489 <vincentcornelius489@gmail.com>
2024-02-16 11:27:23 -0500

    Updated Hero to display on the left

--- components/Hero.vue
+++ components/Hero.vue
@@ -7,22 +7,23 @@
     <div class="overlay absolute inset-0 bg-black opacity-40"></div>
 
     <div class="container mx-auto pt-56">
-      <div class="flex justify-center">
-        <div class="lg:w-2/3 text-center relative">
-          <div class="space-y-6 mb-10">
-            <h2 class="text-white text-4xl md:text-5xl lg:text-6xl leading-tight capitalize font-bold">
-              SIM BOXING LEAGUE
-            </h2>
-            <p class="text-gray-300 text-base md:text-lg">
-              Don't leave it to the judges.
-            </p>
-            <button class="btn">
-              Join Now!
-            </button>
-          </div>
-        </div>
+  <div class="flex justify-start"> <!-- Changed justify-center to justify-start -->
+    <div class="lg:w-1/2 text-center relative"> <!-- Changed lg:w-2/3 to lg:w-1/2 -->
+      <div class="space-y-6 mb-10">
+        <h2 class="text-white text-4xl lg:text-8xl leading-tight capitalize font-semibold">
+          SIM BOXING LEAGUE
+        </h2>
+        <p class="text-gray-300 text-base md:text-lg">
+          Don't leave it to the judges.
+        </p>
+        <button class="btn">
+          Join Now!
+        </button>
       </div>
     </div>
+  </div>
+</div>
+
 
   </section>
 </template>
